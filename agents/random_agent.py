df68da78ee722826271d7b9416cba61cdf1fdd2b
Tze Tat Yao <yaot@Tzes-Air.lan>
2026-01-26 16:38:46 +0800

    Integrated backend and frontend structure

--- agents/random_agent.py
+++
@@ -1,9 +0,0 @@
-from agents.agent import Agent
-import random
-
-class RandomAgent(Agent):
-    def action(self,x_state,o_state,state):
-        actions = self.actions(state)
-        action = random.choice(actions)
-        return action
-
